9fe7b6311a4d6719516e931eb24746525402d29b
v4rts <v4rts@yandex.ru>
2020-08-28 17:12:23 +0300

    added components, YouTubeApi.js, Axios package

--- api/src/App.js
+++ api/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Search } from './components/Search';
+import { List } from './components/List';
+import { Description } from './components/Description';
 
 const App = () => {
     return(
-        <h1></h1>
+        <div ClassName = "wrapper">
+            <Search />
+            <List />
+            <Description />
+        </div>
     )
 }
 export default App;
