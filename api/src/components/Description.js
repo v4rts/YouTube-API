c2173d793ae86f1eedcba06278508b46940ac9b1
v4rts <v4rts@yandex.ru>
2020-09-04 15:07:13 +0300

    description,title,channelTitle added

--- api/src/components/Description.js
+++ api/src/components/Description.js
@@ -14,7 +14,9 @@ const Description = ( {video} ) => {
                 </Paper>
 
                 <Paper elevation = {6} style = {{padding : '15px'}}>
-
+                    <Typography variant="h4">{video.snippet.title}</Typography>
+                    <Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
+                    <Typography variant="subtitle2">{video.snippet.description}</Typography>
                 </Paper>
             </React.Fragment>
         );
