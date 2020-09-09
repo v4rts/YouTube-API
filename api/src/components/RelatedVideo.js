3faba30724511c1f09ac565ec47b1792e09b4dfd
v4rts <v4rts@yandex.ru>
2020-09-09 13:11:01 +0300

    added relatedvideo component

---
+++ api/src/components/RelatedVideo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+
+export default RelatedVideo;
